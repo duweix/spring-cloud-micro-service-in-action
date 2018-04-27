8c805c914f8b9062c732a86cc5890aff193f7cbc
duweix <duweix@126.com>
2018-04-28 00:27:25 +0800

    duweix

---
+++ eureka-client-hello/src/main/java/com/example/eurekaclienthello/EurekaClientHelloApplication.java
@@ -0,0 +1,12 @@
+package com.example.eurekaclienthello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaClientHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientHelloApplication.class, args);
+	}
+}
